21fa7625da6406e0ae2ba96901db7052933de283
Said <saidacostacepeda03@gmail.com>
2025-12-19 22:50:23 -0500

    feat: Implement basic structure

---
+++ src/main/java/com/financialflow/FinancialflowApplication.java
@@ -0,0 +1,13 @@
+package com.financialflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financialflow/FinancialflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

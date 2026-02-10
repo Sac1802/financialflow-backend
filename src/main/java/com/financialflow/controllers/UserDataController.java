875e48aa798a0ab19c687a030506996835506fe7
Said <saidacostacepeda03@gmail.com>
2026-02-09 21:34:13 -0500

    feat: Indexes were implemented to improve response times in database queries.

--- src/main/java/com/financialflow/controllers/UserDataController.java
+++ src/main/java/com/financialflow/controllers/UserDataController.java
@@ -31,14 +31,14 @@ public class UserDataController {
 
     private final UserDataService service;
 
-    public UserDataController(UserDataService service){
+    public UserDataController(UserDataService service) {
         this.service = service;
     }
-    
+
     @Operation(summary = "Create a new user", description = "Creates a new user.")
     @ApiResponse(responseCode = "200", description = "User created successfully")
     @PostMapping
-    public ResponseEntity<?> saveUser(@Valid @RequestBody UserDataDTO data){
+    public ResponseEntity<?> saveUser(@Valid @RequestBody UserDataDTO data) {
         UserDataDTO response = service.saveUserData(data);
         return ResponseEntity.ok(response);
     }
@@ -47,14 +47,14 @@ public class UserDataController {
     @Operation(summary = "Get all users", description = "Retrieves all users from the system.")
     @ApiResponse(responseCode = "200", description = "Users retrieved successfully")
     @GetMapping
-    public ResponseEntity<?> getAllUsers(){
+    public ResponseEntity<?> getAllUsers() {
         return ResponseEntity.ok(service.getUsers());
     }
 
     @Operation(summary = "Get user by ID", description = "Retrieves a user by their ID.")
     @ApiResponse(responseCode = "200", description = "User retrieved successfully")
     @GetMapping("/id")
-    public ResponseEntity<?> getUserById(@AuthenticationPrincipal Integer userId){
+    public ResponseEntity<?> getUserById(@AuthenticationPrincipal Integer userId) {
         UserResponse userFind = service.getUserById(userId);
         return ResponseEntity.ok(userFind);
     }
@@ -62,7 +62,7 @@ public class UserDataController {
     @Operation(summary = "Update a user", description = "Updates an existing user.")
     @ApiResponse(responseCode = "200", description = "User updated successfully")
     @PutMapping
-    public ResponseEntity<?> updateUser(@Valid @RequestBody UserDataDTO user,@AuthenticationPrincipal Integer userId){
+    public ResponseEntity<?> updateUser(@Valid @RequestBody UserDataDTO user, @AuthenticationPrincipal Integer userId) {
         UserResponse response = service.updateUser(userId, user);
         return ResponseEntity.ok(response);
     }
@@ -70,7 +70,8 @@ public class UserDataController {
     @Operation(summary = "Update user password", description = "Updates the password of the authenticated user.")
     @ApiResponse(responseCode = "200", description = "Password updated successfully")
     @PutMapping("/update/password")
-    public ResponseEntity<?> updatePassword(@AuthenticationPrincipal Integer userId,@RequestBody PasswordRequest newPassword){
+    public ResponseEntity<?> updatePassword(@AuthenticationPrincipal Integer userId,
+            @RequestBody PasswordRequest newPassword) {
         String response = service.updatePasswordUser(userId, newPassword);
         return ResponseEntity.ok(response);
     }
@@ -79,7 +80,7 @@ public class UserDataController {
     @Operation(summary = "Validate email", description = "Validates if an email is already in use.")
     @ApiResponse(responseCode = "200", description = "Email validation successful")
     @PostMapping("/validate/email")
-    public ResponseEntity<?> validateEmail(@RequestBody EmailRequest email){
+    public ResponseEntity<?> validateEmail(@RequestBody EmailRequest email) {
         String response = service.verifyEmail(email);
         return ResponseEntity.ok(response);
     }
@@ -87,8 +88,8 @@ public class UserDataController {
     @Operation(summary = "Delete a user", description = "Deletes the authenticated user.")
     @ApiResponse(responseCode = "204", description = "User deleted successfully")
     @DeleteMapping
-    public ResponseEntity<?> deleteUser(@AuthenticationPrincipal Integer userId){
+    public ResponseEntity<?> deleteUser(@AuthenticationPrincipal Integer userId) {
         String response = service.deleteUser(userId);
-        return ResponseEntity.status(204).body(response); 
+        return ResponseEntity.status(204).body(response);
     }
 }
